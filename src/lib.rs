2cc952614e0c6406839ae93c49481363f4d4ddb4
Damian J Pound <damian@thebestisp.com>
2023-03-13 16:53:43 -0500

    Combine rules

--- src/lib.rs
+++ src/lib.rs
@@ -139,17 +139,16 @@ impl Universe {
                 let next_cell = match (cell, live_neighbors) {
                     // Rule 1: Any live cell with fewer than two live neighbours
                     // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
                     // Rule 3: Any live cell with more than three live
                     // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 1+3: Any live cell that doesn't have two or three neighbors dies.
+                    (Cell::Alive, x) if x != 2 && x != 3 => Cell::Dead,
                     // Rule 4: Any dead cell with exactly three live neighbours
                     // becomes a live cell, as if by reproduction.
                     (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
+                    // All other cells remain in the same state. (effectively includes rule 2)
+                    // Rule 2: Any live cell with two or three live neighbours
+                    // lives on to the next generation.
                     (otherwise, _) => otherwise,
                 };
 
